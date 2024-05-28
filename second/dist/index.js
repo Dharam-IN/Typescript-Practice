cd231f3470a14f399db96fa967df6122df542c64
dharam-in <dharamdotin@gmail.com>
2024-05-28 19:16:42 +0530

    Generics in Typescript

--- second/dist/index.js
+++ second/dist/index.js
@@ -2,7 +2,26 @@
 // GET DATA
 // const form = document.getElementById("MyForm") as HTMLFormElement;
 // const input = document.querySelector("form > input") as HTMLInputElement;
-const myobj = {
-    name: "Dharam",
-    email: "dharamdotin@gmail.com"
+const Users = [
+    {
+        name: "Dharam",
+        age: 19,
+    },
+    {
+        name: "Karam",
+        age: 19
+    },
+    {
+        name: "Radhe",
+        age: 22
+    },
+    {
+        name: "Ram",
+        age: 20
+    },
+];
+const filterByPeoples = (arr, property, value) => {
+    return arr.filter((item) => item[property] === value);
 };
+const filterPeopleUsingName = filterByPeoples(Users, "name", "Ram");
+console.log(filterPeopleUsingName);
--- second/src/index.ts
+++ second/src/index.ts
@@ -19,12 +19,80 @@
 
 
 // INTERFACE CREATE
-interface Person{
-    name: String,
-    email: String
+// interface Person{
+//     name: String,
+//     email: String
+// }
+
+// const myobj: Person = {
+//     name: "Dharam",
+//     email: "dharamdotin@gmail.com"
+// }
+
+
+
+
+// UTILITY TYPE
+// ---Partial
+// type Person = {
+//     name: string,
+//     email: string
+// }
+
+// type Person2 = Partial<Person>
+
+
+// ---  Required - Opposite of Partial
+// type Person = {
+//     name?: string,
+//     email: string
+// }
+
+
+// type Person2 = Required<Person>
+
+
+
+
+
+
+
+// GENERICS IN TYPESCRIPT
+// const func = <CustomType>(n: CustomType): CustomType => {
+//     return n;
+// }
+
+// const ans = func(5);
+// const ans2 = func("india");
+// const ans3 = func(true);
+
+type Person = {
+    name: string,
+    age: number
+};
+
+const Users: Person[] = [
+    {
+        name: "Dharam",
+        age: 19,
+    },
+    {
+        name: "Karam",
+        age: 19
+    },
+    {
+        name: "Radhe",
+        age: 22
+    },
+    {
+        name: "Ram",
+        age: 20
+    },
+]
+
+const filterByPeoples = <T, U extends keyof T>(arr: T[], property: U, value: T[U]): T[] => {
+    return arr.filter((item) => item[property] === value)
 }
 
-const myobj: Person = {
-    name: "Dharam",
-    email: "dharamdotin@gmail.com"
-}
+const filterPeopleUsingName = filterByPeoples(Users, "name", "Ram")
+console.log(filterPeopleUsingName)
