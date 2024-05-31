6950a56f4ab1211812c0f521cc24ba57f1020e4f
dharam-in <dharamdotin@gmail.com>
2024-05-31 20:00:32 +0530

    UseState hook in typescript

--- 04_day/src/App.tsx
+++ 04_day/src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import Box from './Components/Box'
+import { FormEvent, useState } from "react"
 
 function App() {
-  const [val, setVal] = useState<string>("");
+
+  interface Person{
+    name: string,
+    age: number
+  }
+
+  const [user, setUser] = useState<Person>();
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(user)
+  }
 
   return (
     <>
-      <Box label='Search' value={val} setter={setVal}/>
+      <form onSubmit={submitHandler}>
+        <label>User</label>
+        <input type="number" value={user?.age} onChange={(e) => setUser((prev) => ({...prev!, age: Number(e.target.value)}))} />
+        <input type="text" value={user?.name} onChange={(e) => setUser((prev) => ({...prev!, name: e.target.value}))} />
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
